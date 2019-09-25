d49456f36d1b2fb00d6baebc4a71418e9ff4e831
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-09-24 21:10:16 -0400

    get pieces in order for specifying minister/critic

--- load.R
+++ load.R
@@ -37,16 +37,19 @@ lop_mps <- import_lop_mps %>%
   mutate(days_of_service = as.numeric(str_extract(years_of_service, "(\\d+)"))) %>%
   mutate(years_of_service = days_of_service / 365)
 
-## Separate "Type of Parliamentarian" column into multiple rows per period of MP's service
-lop_mps <- lop_mps %>%
-  separate_rows(role_type_of_parliamentarian, seat_riding_senatorial_division, sep="\\)") %>%
-  filter(role_type_of_parliamentarian != "")
+lop_mps_fixed_vars <- lop_mps %>%
+  select(
+    id,
+    name:deceased_date,
+    gender:profession,
+    military_service,
+    date_of_parliamentary_entry,
+    years_of_service,
+    days_of_service
+  )
 
-by_service <- import_lop_mps %>%
-  mutate(role_type_of_parliamentarian = list(head(str_split(role_type_of_parliamentarian, "\\)")[[1]], -1)))
-
-## TODO: extract type of service, service start, service end for each record
-
+# Determine geographies for each type of service for each MP
+# (`geography_id` corresponds to `role_id` in `lop_mps_role_type_of_parliamentarian_by_role`)
 lop_mps_seat_riding_senatorial_division <- lop_mps %>%
   select(id, geography = seat_riding_senatorial_division) %>%
   separate_rows(geography, sep="(?<=[ 0-9])(\\))") %>%
@@ -68,6 +71,7 @@ lop_mps_seat_riding_senatorial_division <- lop_mps %>%
   ungroup() %>%
   select(geography_id, geography)
 
+# Separate "Type of Parliamentarian" column into multiple rows per period of MP's service
 lop_mps_role_type_of_parliamentarian_by_role <- lop_mps %>%
   select(id, role = role_type_of_parliamentarian) %>%
   separate_rows(role, sep="\\)") %>%
@@ -90,6 +94,7 @@ lop_mps_role_type_of_parliamentarian_by_role <- lop_mps %>%
   select(id, role_id, role, period_start, period_end) %>%
   left_join(lop_mps_seat_riding_senatorial_division, by = c("role_id" = "geography_id"))
 
+# Explode roles into days!
 lop_mps_role_type_of_parliamentarian_by_role_by_day <- lop_mps_role_type_of_parliamentarian_by_role %>%
   gather(period_start, period_end, key = "period_bound", value = "date") %>%
   select(id, role_id, role, date) %>%
@@ -98,9 +103,79 @@ lop_mps_role_type_of_parliamentarian_by_role_by_day <- lop_mps_role_type_of_parl
   complete(date = full_seq(date, 1), nesting(id, role)) %>%
   ungroup()
 
+
+# Explode period-bound vars (`political_affiliation`, `role_minister`, `role_critic`)
+# into their own tables, like `lop_mps_role_type_of_parliamentarian_by_role`. Then, in
+# the per-day table, find the relevant period bounds for each day and assign accordingly.
+
+### `role_minister`
+lop_mps_role_minister_by_position <- lop_mps %>%
+  select(id, minister = role_minister) %>%
+  separate_rows(minister, sep="(?<=[ 0-9])(\\))") %>%
+  filter(minister != "") %>%
+  separate(minister, c("minister", "period"), sep = " (\\()(?=[0-9])") %>%
+  separate(period, c("period_start", "period_end"), sep = " - ") %>%
+  mutate(period_end_nchar = nchar(period_end), period_end = case_when(
+    period_end_nchar == 0 ~ as.character(today()),
+    period_end_nchar == 4 ~ paste0(period_end, "/01/01"),
+    TRUE ~ period_end
+  )) %>%
+  mutate(
+    period_start = ymd(period_start),
+    period_end = ymd(period_end)
+  ) %>%
+  arrange(id, period_start) %>%
+  group_by(id) %>%
+  mutate(minister_id = paste0(id, "-", row_number())) %>%
+  ungroup() %>%
+  select(id, minister_id, minister, period_start, period_end)
+
+### `role_critic`
+#### Warning messages:
+#### 1: Expected 2 pieces. Missing pieces filled with `NA` in 2 rows [1219, 3324]. 
+#### 2:  1009 failed to parse. 
+#### 3:  11 failed to parse. 
+lop_mps_role_critic_by_position <- lop_mps %>%
+  select(id, critic = role_critic) %>%
+  separate_rows(critic, sep="(?<=[ 0-9])(\\))") %>%
+  filter(critic != "") %>%
+  separate(critic, c("critic", "period"), sep = " (\\()(?=[0-9])") %>%
+  separate(period, c("period_start", "period_end"), sep = " - ") %>%
+  mutate(period_end_nchar = nchar(period_end), period_end = case_when(
+    period_end_nchar == 0 ~ "FIXME",
+    period_end_nchar == 4 ~ paste0(period_end, "/01/01"),
+    TRUE ~ period_end
+  )) %>%
+  mutate(period_start_nchar = nchar(period_start), period_start = case_when(
+    period_start_nchar == 0 ~ "FIXME",
+    period_start_nchar == 4 ~ paste0(period_start, "/01/01"),
+    period_start_nchar == 6 ~ paste0(period_start, "/01"),
+    TRUE ~ period_start
+  )) %>%
+  mutate(
+    period_start = ymd(period_start),
+    period_end = ymd(period_end)
+  ) %>%
+  arrange(id, period_start) %>%
+  group_by(id) %>%
+  mutate(critic_id = paste0(id, "-", row_number())) %>%
+  ungroup() %>%
+  select(id, critic_id, critic, period_start, period_end)
+
+
+
+
+# Visualizing
+
+lop_mps_role_type_of_parliamentarian_by_role %>%
+  left_join(lop_mps_fixed_vars) %>%
+  View()
+
 lop_mps_by_day <- lop_mps_role_type_of_parliamentarian_by_role_by_day %>%
   arrange(id) %>%
-  left_join(lop_mps) %>% View()
+  left_join(lop_mps_fixed_vars) %>% View()
+
+
   
 ## dates are `nchar`:
 ### 13 (current parliament, round to today)
