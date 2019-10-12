5cabedd3fb74113b20f05129592e3977617d284c
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-10-11 23:26:32 -0400

    Properly calculate age at first election
    Closes #8, #9.
    
    Verify that MP suggested by Doug has an age of 19, not 20:
    
    ```
    parliamentarians %>%
      left_join(election_candidates %>%
                  group_by(Person.PersonId) %>%
                  filter(IsWin) %>%
                  summarize(date_of_first_election = min(ElectionDate))) %>%
      mutate(age_at_first_election = interval(Person.DateOfBirth, date_of_first_election) / years(1)) %>%
      filter(Person.PersonId == 17944) %>%
      select(age_at_first_election)
    ```

--- analysis/age-at-election.R
+++ analysis/age-at-election.R
@@ -1,36 +1,6 @@
 parliamentarians %>%
-  slice(1:5) %>%
-  mutate(
-    date_of_first_election = map(
-      Person.ElectionCandidates,
-      ~ (.) %>%
-        filter(IsWin) %>%
-        summarize(earliest = min(ElectionDate)) %>%
-        pull(earliest)
-    )
-  ) %>%
-  select(Person.PersonId, date_of_first_election) %>%
-  unnest()
-
-parliamentarians %>%
-  slice(9) %>%
-  mutate(
-    Person.ElectionCandidatesIsEmpty = map_lgl(
-      Person.ElectionCandidates,
-      ~ (.) %>%
-        is_empty()
-    ),
-    date_of_first_election = if_else(
-      Person.ElectionCandidatesIsEmpty,
-      list(NA),
-      map(
-        Person.ElectionCandidates,
-        function (ecs) ecs %>%
-          filter(IsWin) %>%
-          summarize(earliest = min(ElectionDate)) %>%
-          pull(earliest)
-      )
-    )
-  ) %>%
-  select(Person.PersonId, Person.ElectionCandidatesIsEmpty, date_of_first_election) %>%
-  unnest(cols = date_of_first_election)
+  left_join(election_candidates %>%
+              group_by(Person.PersonId) %>%
+              filter(IsWin) %>%
+              summarize(date_of_first_election = min(ElectionDate))) %>%
+  mutate(age_at_first_election = interval(Person.DateOfBirth, date_of_first_election) / years(1))
