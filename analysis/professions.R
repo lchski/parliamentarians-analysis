4095e11b0e78943a71a7f008968ca8c955344b7a
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-10-13 22:27:45 -0400

    frequency poly with professions!

---
+++ analysis/professions.R
@@ -0,0 +1,15 @@
+by_profession <- professions %>%
+  select(Person.PersonId, NameEn, ProfessionTypeEn) %>%
+  left_join(by_age_at_first_election)
+
+by_profession %>%
+  filter(ProfessionTypeEn %in% (by_profession %>%
+           count_group(ProfessionTypeEn) %>%
+           slice(1:8) %>%
+           pull(ProfessionTypeEn))) %>%
+  mutate(decade_of_first_election = floor_date(date_of_first_election, years(10))) %>%
+  ggplot(aes(x = decade_of_first_election, stat(density), color = Person.Gender)) +
+  geom_freqpoly(bins = 15) +
+  facet_wrap(vars(ProfessionTypeEn), ncol = 2, labeller = label_wrap_gen()) +
+  theme(strip.text.x = element_text(hjust = 0)) +
+  xlim(as.Date(c("1860-01-01", "2020-01-01")))
