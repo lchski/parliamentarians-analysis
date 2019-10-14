e778f169a4d1344faa3c9317ba8703b82b06cf80
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-10-13 22:42:19 -0400

    some more experimenting with different visualizations

--- analysis/professions.R
+++ analysis/professions.R
@@ -13,3 +13,29 @@ by_profession %>%
   facet_wrap(vars(ProfessionTypeEn), ncol = 2, labeller = label_wrap_gen()) +
   theme(strip.text.x = element_text(hjust = 0)) +
   xlim(as.Date(c("1860-01-01", "2020-01-01")))
+
+by_profession %>%
+  filter(NameEn %in% (by_profession %>%
+                                  count_group(NameEn) %>%
+                                  slice(1:12) %>%
+                                  pull(NameEn))) %>%
+  mutate(decade_of_first_election = floor_date(date_of_first_election, years(10))) %>%
+  ggplot(aes(x = decade_of_first_election, stat(density), color = Person.Gender)) +
+  geom_freqpoly(bins = 15) +
+  facet_wrap(vars(NameEn), ncol = 2, labeller = label_wrap_gen()) +
+  theme(strip.text.x = element_text(hjust = 0)) +
+  xlim(as.Date(c("1860-01-01", "2020-01-01")))
+
+
+
+by_profession %>%
+  filter(ProfessionTypeEn %in% (by_profession %>%
+                        count_group(ProfessionTypeEn) %>%
+                        slice(1:8) %>%
+                        pull(ProfessionTypeEn))) %>%
+  mutate(decade_of_first_election = floor_date(date_of_first_election, years(10))) %>%
+  ggplot(aes(x = decade_of_first_election, fill = Person.Gender)) + ## TODO figure out why the `fill` changes the %s in `y`
+  geom_bar(aes(y = ..prop..)) +
+  facet_wrap(vars(ProfessionTypeEn), ncol = 2, labeller = label_wrap_gen()) +
+  theme(strip.text.x = element_text(hjust = 0)) +
+  xlim(as.Date(c("1860-01-01", "2020-01-01")))
