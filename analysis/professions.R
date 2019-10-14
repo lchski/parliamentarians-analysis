1dda95aa6d867d9dcf09abf6fae88103d2ff4919
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-10-13 22:53:06 -0400

    bar charts on %, not #

--- analysis/professions.R
+++ analysis/professions.R
@@ -34,8 +34,20 @@ by_profession %>%
                         slice(1:8) %>%
                         pull(ProfessionTypeEn))) %>%
   mutate(decade_of_first_election = floor_date(date_of_first_election, years(10))) %>%
-  ggplot(aes(x = decade_of_first_election, fill = Person.Gender)) + ## TODO figure out why the `fill` changes the %s in `y`
-  geom_bar(aes(y = ..prop..)) +
+  ggplot(aes(x = decade_of_first_election)) + ## TODO figure out why the `fill` with `Person.Gender` changes the %s in `y`
+  geom_bar(aes(y = ..prop.., group = 1)) +
   facet_wrap(vars(ProfessionTypeEn), ncol = 2, labeller = label_wrap_gen()) +
+  theme(strip.text.x = element_text(hjust = 0)) +
+  xlim(as.Date(c("1860-01-01", "2020-01-01")))
+
+by_profession %>%
+  filter(NameEn %in% (by_profession %>%
+                                  count_group(NameEn) %>%
+                                  slice(1:8) %>%
+                                  pull(NameEn))) %>%
+  mutate(decade_of_first_election = floor_date(date_of_first_election, years(10))) %>%
+  ggplot(aes(x = decade_of_first_election)) + ## TODO figure out why the `fill` with `Person.Gender` changes the %s in `y`
+  geom_bar(aes(y = ..prop.., group = 1)) +
+  facet_wrap(vars(NameEn), ncol = 2, labeller = label_wrap_gen()) +
   theme(strip.text.x = element_text(hjust = 0)) +
   xlim(as.Date(c("1860-01-01", "2020-01-01")))
