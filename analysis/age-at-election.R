51d96cb0d601154d63c1d31a5f43a6ccbd0fc64a
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-10-13 21:58:44 -0400

    replicate my plots from last year woooo

--- analysis/age-at-election.R
+++ analysis/age-at-election.R
@@ -1,6 +1,52 @@
-parliamentarians %>%
+by_age_at_first_election <- parliamentarians %>%
   left_join(election_candidates %>%
               group_by(Person.PersonId) %>%
               filter(IsWin) %>%
               summarize(date_of_first_election = min(ElectionDate))) %>%
-  mutate(age_at_first_election = interval(Person.DateOfBirth, date_of_first_election) / years(1))
+  mutate(age_at_first_election = interval(Person.DateOfBirth, date_of_first_election) / years(1)) %>%
+  select(
+    Person.PersonId,
+    Person.Gender,
+    date_of_first_election,
+    age_at_first_election
+  )
+
+## Histogram of age of first election
+by_age_at_first_election %>%
+  ggplot(aes(x = age_at_first_election)) +
+  geom_histogram(binwidth = 2) +
+  scale_x_continuous(limits = c(0, NA)) +
+  labs(
+    title = "Age at first election to the House of Commons",
+    x = "Age at first election",
+    y = "Count",
+    caption = "By @lchski with data from Library of Parliament."
+  )
+
+## Frequency polygram of age at first election by gender (freqpoly allows comparison between categorical data, i.e. gender)
+by_age_at_first_election %>%
+  ggplot(aes(x = age_at_first_election, stat(density), colour = Person.Gender)) +
+  geom_freqpoly(binwidth = 2) +
+  labs(
+    title = "Age at first election to the House of Commons by gender",
+    x = "Age at first election",
+    y = "Density",
+    caption = "By @lchski with data from Library of Parliament."
+  )
+
+by_age_at_first_election %>%
+  ggplot(aes(x = date_of_first_election, y = age_at_first_election)) +
+  geom_point() +
+  geom_smooth()
+
+by_age_at_first_election %>%
+  ggplot(aes(x = date_of_first_election, y = age_at_first_election)) +
+  geom_hex() +
+  geom_smooth(method = lm, se = FALSE) +
+  facet_wrap(vars(Person.Gender)) +
+  labs(
+    title = "Date of first election to the House of Commons by age at first election",
+    x = "Date of first election",
+    y = "Age at first election",
+    caption = "By @lchski with data from Library of Parliament."
+  )
