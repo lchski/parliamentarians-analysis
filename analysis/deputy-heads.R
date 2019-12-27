e088d32f50fb378acbee9534e5bbe631c5225e30
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-12-27 12:09:09 -0500

    start to look at deputy heads

---
+++ analysis/deputy-heads.R
@@ -0,0 +1,76 @@
+source("scripts/load-institutions.R")
+
+## list current DHs
+dhs_current <- deputy_heads %>%
+  filter(IsCurrent)
+
+dhs_current %>%
+  count_group(OrganizationNameEn, PortFolioEn, ToBeStyledAsEn)
+
+dhs_current_ids <- dhs_current %>% pull(PersonId) %>% unique()
+
+deputy_heads %>%
+  filter(PersonId %in% dhs_current_ids)
+
+## visualize years in role over time
+deputy_heads %>%
+  select(StartDate, years_in_role_raw, Gender) %>%
+  filter(Gender != "") %>%
+  mutate(StartDateDecade = floor_date(StartDate, unit = "10 years")) %>%
+  ggplot(aes(x = StartDate, y = years_in_role_raw)) +
+  geom_point() +
+  geom_smooth() +
+  facet_grid(~ Gender)
+
+deputy_heads %>%
+  select(PersonId, Gender) %>%
+  distinct() %>%
+  count_group(Gender) %>%
+  mutate(prop = count / sum(count))
+
+deputy_heads %>%
+  select(PersonId, Gender, years_in_role) %>%
+  filter(years_in_role >= 0) %>%
+  group_by(PersonId, Gender) %>%
+  summarize(years_as_dh = sum(years_in_role)) %>%
+  group_by(Gender) %>%
+  filter(Gender != "") %>%
+  summarize(count = n(), years = sum(years_as_dh)) %>%
+  mutate(count_prop = count / sum(count), years_prop = years / sum(years))
+
+find_closest_date <- function(date_to_place, dates_to_compare) {
+  tibble(date = dates_to_compare, source = "base") %>%
+    bind_rows(list(date = date_to_place, source = "comparator")) %>%
+    arrange(date) %>%
+    mutate(previous_date = lag(date)) %>%
+    filter(source == "comparator") %>%
+    pull(previous_date)
+}
+
+find_closest_date_vectorized <- Vectorize(find_closest_date)
+
+deputy_heads %>%
+  mutate(closest_ministry_date = find_closest_date_vectorized(StartDate, ministries$start_date))
+
+deputy_heads %>%
+  mutate(closest_ministry_date = map_int(StartDate, find_closest_date(., ministries$start_date)))
+
+deputy_heads %>%
+  mutate(closest_ministry_date = find_closest_date_vectorized(StartDate, ministries$start_date)) %>% filter(is.na(closest_ministry_date)) %>% View()
+
+deputy_heads %>%
+  mutate(closest_ministry_date = map_int(StartDate, find_closest_date(., ministries$start_date)))
+
+deputy_heads %>%
+  mutate(closest_ministry_date = map_dbl(StartDate, ~ find_closest_date(., ministries$start_date))) %>%
+  mutate(closest_ministry_date = as_date(closest_ministry_date))
+
+find_closest_date(date("1988-09-15"), ministries$start_date)
+
+tibble(date = ministries$start_date, source = "base") %>%
+  bind_rows(list(date = date("1994-05-09"), source = "comparator")) %>%
+  arrange(date) %>%
+  mutate(previous_date = lag(date)) %>%
+  filter(source == "comparator") %>%
+  pull(previous_date)
+  View()
---
+++ scripts/load-institutions.R
@@ -0,0 +1,76 @@
+departmental_roles <- fromJSON("data/institutions/departments.json", simplifyDataFrame = TRUE) %>%
+  as_tibble() %>%
+  remove_extra_columns() %>%
+
+departmental_roles_extra_roles <- departmental_roles %>%
+  select(ExtraRoles) %>% ## probably want an ID variable or something to be able to connect back to `departmental_roles`
+  unnest(cols = c(ExtraRoles))
+
+departmental_roles <- departmental_roles %>%
+  select(-ExtraRoles) %>%
+  mutate_at(
+    c(
+      "OrganizationStartDate",
+      "OrganizationEndDate",
+      "StartDate",
+      "EndDate"
+    ),
+    date
+  ) %>%
+  mutate(
+    IsCurrent = is.na(EndDate),
+    EndDate = case_when(
+      IsCurrent ~ today(),
+      TRUE ~ EndDate
+    ),
+    period_in_role = interval(StartDate, EndDate),
+    years_in_role = time_length(period_in_role, unit = "years")
+  ) %>%
+  mutate_at(
+    c(
+      "PortFolioEn"
+    ),
+    trimws
+  )
+
+remove_deputy_head_duplicates <- function(dhs) {
+  dhs %>%
+    select(-Parliament:-ParliamentEndDate) %>%
+    distinct()
+}
+
+## for use if you want to analyse DHs surviving parliaments
+deputy_heads_raw <- departmental_roles %>%
+  filter(! NameEn %in% c(
+    "Minister",
+    "Parliamentary Secretary",
+    "Critic",
+    "Assistant Critic",
+    "Minister of State",
+    "Parliamentary Assistant",
+    "Associate Minister",
+    "Secretary of State"
+  )) %>%
+  remove_extra_columns()
+
+deputy_heads <- deputy_heads_raw %>%
+  remove_deputy_head_duplicates() %>%
+  arrange(OrganizationId, PortFolioEn, StartDate) %>%
+  group_by(OrganizationId, PortFolioEn, NameEn) %>%
+  mutate(EndDateRaw = EndDate) %>%
+  mutate(EndDate = case_when(
+    EndDateIsApproximate ~ lead(StartDate) - days(1),
+    TRUE ~ EndDate
+  )) %>%
+  ungroup() %>%
+  select(OrganizationId:EndDate, EndDateRaw, EndDateIsApproximate:years_in_role) %>%
+  rename(
+    period_in_role_raw = period_in_role,
+    years_in_role_raw = years_in_role
+  ) %>%
+  mutate(
+    period_in_role = interval(StartDate, EndDate),
+    years_in_role = time_length(period_in_role, unit = "years")
+  )
+
+
